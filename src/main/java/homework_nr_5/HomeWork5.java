69807d978076b6182a5a8f93f08d1a311700871a
jeca <mr.jenia11@gmail.com>
2024-07-24 14:51:30 +0300

    Home Work 5

--- src/main/java/homework_nr_5/HomeWork5.java
+++ src/main/java/homework_nr_5/HomeWork5.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class HomeWork5 {
     public static void main(String[] args) {
 
+
         int[] numbers = new int[100];
         int sum = 0;
         long prod = 1;
